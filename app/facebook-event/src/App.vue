755bc70a14d113a5e3de5bcc64bd30b8896e79f3
Takuro <fantasistakuro@gmail.com>
2019-09-01 01:03:56 +0900

    bulmaの追加

--- app/facebook-event/src/App.vue
+++ app/facebook-event/src/App.vue
@@ -11,6 +11,8 @@
 </template>
 
 <style lang="scss">
+@import '../node_modules/bulma/bulma.sass';
+
 #app {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
--- app/facebook-event/src/views/Count.vue
+++ app/facebook-event/src/views/Count.vue
@@ -2,9 +2,9 @@
   <div class="count">
     <h1>This is count page</h1>
     {{ showCounter }}
-    <button @click="increment">+</button>
-    <button @click="incrementAsync">++</button>
-    <button @click="decrement">-</button>
+    <button class="button is-primary" @click="increment">+</button>
+    <button class="button is-secondary" @click="incrementAsync">++</button>
+    <button class="button is-danger" @click="decrement">-</button>
   </div>
 </template>
 
