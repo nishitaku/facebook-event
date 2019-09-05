0a2a6da292b46ffeaf3c9eeff0befd5847c2dcba
Takuro <fantasistakuro@gmail.com>
2019-09-05 18:26:26 +0900

    firebase連携

--- Dockerfile
+++ Dockerfile
@@ -3,4 +3,5 @@ FROM node:lts-alpine
 WORKDIR /app
 
 RUN apk update && \
-  npm i -g @vue/cli
+  npm i -g @vue/cli && \
+  npm i -g firebase-tools
--- app/facebook-event/src/main.ts
+++ app/facebook-event/src/main.ts
@@ -1,3 +1,4 @@
+import firebase from 'firebase';
 import Vue from 'vue';
 
 import App from './App.vue';
@@ -5,6 +6,18 @@ import router from './router';
 import store from './store/store';
 
 Vue.config.productionTip = false;
+
+const firebaseConfig = {
+  apiKey: process.env.VUE_APP_FIREBASE_API_KEY,
+  authDomain: process.env.VUE_APP_FIREBASE_AUTH_DOMAIN,
+  databaseURL: process.env.VUE_APP_FIREBASE_DATABASE_URL,
+  projectId: process.env.VUE_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.VUE_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.VUE_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.VUE_APP_FIREBASE_APP_ID
+};
+
+firebase.initializeApp(firebaseConfig);
 
 new Vue({
   router,
