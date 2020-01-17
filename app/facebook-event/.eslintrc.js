d6e1bf0d302bb2939b52da026585df1195e7247a
Takuro Nishino <takuro@Takubook-2.local>
2020-01-18 00:44:57 +0900

    依存ライブラリのアップデート

--- app/facebook-event/.eslintrc.js
+++ app/facebook-event/.eslintrc.js
@@ -4,7 +4,7 @@ module.exports = {
     node: true
   },
   extends: ['plugin:vue/essential', '@vue/prettier', '@vue/typescript'],
-  plugins: ['simple-import-sort'],
+  plugins: ['simple-import-sort', '@typescript-eslint'],
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
